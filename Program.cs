b2b2c90eaee7b695b462af9426e67c5148e494bf
Theloopes <theloopesbr@gmail.com>
2023-08-26 08:27:26 -0300

    Exercicio resolvido.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Troca de valores");
+
+Console.WriteLine("Digite um nome.");
+string nome1 = Console.ReadLine()!;
+Console.WriteLine("Digite outro nome.");
+string nome2 = Console.ReadLine()!;
+Console.WriteLine("");
+
+Console.WriteLine($"Primeiro nome: {nome1}");
+Console.WriteLine($"Segundo nome: {nome2}");
+Console.WriteLine("");
+Console.WriteLine("Após trocar os valores temos:");
+Console.WriteLine("");
+Console.WriteLine($"Primeiro nome: {nome2}");
+Console.WriteLine($"Segundo nome: {nome1}");
